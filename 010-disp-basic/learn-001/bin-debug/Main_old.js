63c7f24f50658e4cc8262ad9b54cedfbc153018d
50212025 <hottredpen@126.com>
2018-09-03 13:53:31 +0800

    手写完成

--- 010-disp-basic/learn-001/bin-debug/Main_old.js
+++ 010-disp-basic/learn-001/bin-debug/Main_old.js
@@ -1,3 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-present, Egret Technology.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
 var __reflect = (this && this.__reflect) || function (p, c, t) {
     p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
 };
@@ -59,10 +87,11 @@ var Main = (function (_super) {
         egret.lifecycle.onResume = function () {
             egret.ticker.resume();
         };
-        // 注入自定义的素材解析器
+        //inject the custom material parser
+        //注入自定义的素材解析器
         var assetAdapter = new AssetAdapter();
-        egret.registerImplementation('eui.IAssetAdapter', assetAdapter);
-        egret.registerImplementation('eui.IThemeAdapter', new ThemeAdapter());
+        egret.registerImplementation("eui.IAssetAdapter", assetAdapter);
+        egret.registerImplementation("eui.IThemeAdapter", new ThemeAdapter());
         this.runGame().catch(function (e) {
             console.log(e);
         });
@@ -75,8 +104,8 @@ var Main = (function (_super) {
                     case 0: return [4 /*yield*/, this.loadResource()];
                     case 1:
                         _a.sent();
-                        this.createGameSence();
-                        return [4 /*yield*/, RES.getResAsync('description_json')];
+                        this.createGameScene();
+                        return [4 /*yield*/, RES.getResAsync("description_json")];
                     case 2:
                         result = _a.sent();
                         this.startAnimation(result);
@@ -101,20 +130,20 @@ var Main = (function (_super) {
                         _a.trys.push([0, 4, , 5]);
                         loadingView = new LoadingUI();
                         this.stage.addChild(loadingView);
-                        return [4 /*yield*/, RES.loadConfig('resource/default.res.json', 'resource/')];
+                        return [4 /*yield*/, RES.loadConfig("resource/default.res.json", "resource/")];
                     case 1:
                         _a.sent();
                         return [4 /*yield*/, this.loadTheme()];
                     case 2:
                         _a.sent();
-                        return [4 /*yield*/, RES.loadGroup('preload', 0, loadingView)];
+                        return [4 /*yield*/, RES.loadGroup("preload", 0, loadingView)];
                     case 3:
                         _a.sent();
                         this.stage.removeChild(loadingView);
                         return [3 /*break*/, 5];
                     case 4:
                         e_1 = _a.sent();
-                        console.log(e_1);
+                        console.error(e_1);
                         return [3 /*break*/, 5];
                     case 5: return [2 /*return*/];
                 }
@@ -122,20 +151,22 @@ var Main = (function (_super) {
         });
     };
     Main.prototype.loadTheme = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var _this = this;
-            return __generator(this, function (_a) {
-                return [2 /*return*/, new Promise(function (resolve, reject) {
-                        var theme = new eui.Theme('resource/default.thm.json', _this.stage);
-                        theme.addEventListener(eui.UIEvent.COMPLETE, function () {
-                            resolve();
-                        }, _this);
-                    })];
-            });
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+            //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+            var theme = new eui.Theme("resource/default.thm.json", _this.stage);
+            theme.addEventListener(eui.UIEvent.COMPLETE, function () {
+                resolve();
+            }, _this);
         });
     };
-    Main.prototype.createGameSence = function () {
-        var sky = this.createBitmapByName('bg_jpg');
+    /**
+     * 创建场景界面
+     * Create scene interface
+     */
+    Main.prototype.createGameScene = function () {
+        var sky = this.createBitmapByName("bg_jpg");
         this.addChild(sky);
         var stageW = this.stage.stageWidth;
         var stageH = this.stage.stageHeight;
@@ -147,7 +178,7 @@ var Main = (function (_super) {
         topMask.graphics.endFill();
         topMask.y = 33;
         this.addChild(topMask);
-        var icon = this.createBitmapByName('egret_icon_png');
+        var icon = this.createBitmapByName("egret_icon_png");
         this.addChild(icon);
         icon.x = 26;
         icon.y = 33;
@@ -159,15 +190,15 @@ var Main = (function (_super) {
         line.x = 172;
         line.y = 61;
         this.addChild(line);
-        var colorLable = new egret.TextField();
-        colorLable.textColor = 0xffffff;
-        colorLable.width = stageW - 172;
-        colorLable.textAlign = 'center';
-        colorLable.text = 'hello egret';
-        colorLable.size = 24;
-        colorLable.x = 172;
-        colorLable.y = 80;
-        this.addChild(colorLable);
+        var colorLabel = new egret.TextField();
+        colorLabel.textColor = 0xffffff;
+        colorLabel.width = stageW - 172;
+        colorLabel.textAlign = "center";
+        colorLabel.text = "Hello Egret";
+        colorLabel.size = 24;
+        colorLabel.x = 172;
+        colorLabel.y = 80;
+        this.addChild(colorLabel);
         var textfield = new egret.TextField();
         this.addChild(textfield);
         textfield.alpha = 0;
@@ -179,29 +210,30 @@ var Main = (function (_super) {
         textfield.y = 135;
         this.textfield = textfield;
         var button = new eui.Button();
-        button.label = 'Click';
+        button.label = "Click!";
         button.horizontalCenter = 0;
         button.verticalCenter = 0;
         this.addChild(button);
         button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
     };
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
     Main.prototype.createBitmapByName = function (name) {
         var result = new egret.Bitmap();
         var texture = RES.getRes(name);
         result.texture = texture;
         return result;
     };
-    Main.prototype.onButtonClick = function (e) {
-        var panel = new eui.Panel();
-        panel.title = 'Title';
-        panel.horizontalCenter = 0;
-        panel.verticalCenter = 0;
-        this.addChild(panel);
-    };
-    Main.prototype.startAnimation = function (resule) {
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    Main.prototype.startAnimation = function (result) {
         var _this = this;
         var parser = new egret.HtmlTextParser();
-        var textflowArr = resule.map(function (text) { return parser.parse(text); });
+        var textflowArr = result.map(function (text) { return parser.parse(text); });
         var textfield = this.textfield;
         var count = -1;
         var change = function () {
@@ -211,15 +243,28 @@ var Main = (function (_super) {
             }
             var textFlow = textflowArr[count];
             // 切换描述内容
+            // Switch to described content
             textfield.textFlow = textFlow;
             var tw = egret.Tween.get(textfield);
-            tw.to({ 'alpha': 1 }, 200);
+            tw.to({ "alpha": 1 }, 200);
             tw.wait(2000);
-            tw.to({ 'alpha': 0 }, 200);
+            tw.to({ "alpha": 0 }, 200);
             tw.call(change, _this);
         };
+        change();
+    };
+    /**
+     * 点击按钮
+     * Click the button
+     */
+    Main.prototype.onButtonClick = function (e) {
+        var panel = new eui.Panel();
+        panel.title = "Title";
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        this.addChild(panel);
     };
     return Main;
 }(eui.UILayer));
 __reflect(Main.prototype, "Main");
-//# sourceMappingURL=Main.js.map
+//# sourceMappingURL=Main_old.js.map
--- 010-disp-basic/learn-001/src/Main.ts
+++ 010-disp-basic/learn-001/src/Main.ts
@@ -1,39 +1,10 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
+class Main extends eui.UILayer{
 
-class Main extends eui.UILayer {
+    private textfield:egret.TextField;
 
-
-    protected createChildren(): void {
+    protected createChildren():void{
         super.createChildren();
-
-        egret.lifecycle.addLifecycleListener((context) => {
+        egret.lifecycle.addLifecycleListener((context)=>{
             // custom lifecycle plugin
         })
 
@@ -41,103 +12,93 @@ class Main extends eui.UILayer {
             egret.ticker.pause();
         }
 
-        egret.lifecycle.onResume = () => {
+        egret.lifecycle.onResume = ()=>{
             egret.ticker.resume();
         }
 
-        //inject the custom material parser
-        //注入自定义的素材解析器
+        // 注入自定义的素材解析器
         let assetAdapter = new AssetAdapter();
-        egret.registerImplementation("eui.IAssetAdapter", assetAdapter);
-        egret.registerImplementation("eui.IThemeAdapter", new ThemeAdapter());
+        egret.registerImplementation('eui.IAssetAdapter',assetAdapter);
+        egret.registerImplementation('eui.IThemeAdapter',new ThemeAdapter());
 
-
-        this.runGame().catch(e => {
+        this.runGame().catch(e=>{
             console.log(e);
-        })
+        });
+
     }
 
-    private async runGame() {
+    private async runGame(){
         await this.loadResource()
-        this.createGameScene();
-        const result = await RES.getResAsync("description_json")
+        this.createGameSence();
+        const result = await RES.getResAsync('description_json')
         this.startAnimation(result);
         await platform.login();
         const userInfo = await platform.getUserInfo();
-        console.log(userInfo);
-
+        console.log(userInfo)
     }
 
-    private async loadResource() {
-        try {
+    private async loadResource(){
+        try{
             const loadingView = new LoadingUI();
             this.stage.addChild(loadingView);
-            await RES.loadConfig("resource/default.res.json", "resource/");
+            await RES.loadConfig('resource/default.res.json','resource/');
             await this.loadTheme();
-            await RES.loadGroup("preload", 0, loadingView);
-            this.stage.removeChild(loadingView);
+            await RES.loadGroup('preload',0,loadingView);
+            this.stage.removeChild(loadingView)
         }
-        catch (e) {
-            console.error(e);
+        catch(e){
+            console.log(e)
         }
+
     }
 
-    private loadTheme() {
-        return new Promise((resolve, reject) => {
-            // load skin theme configuration file, you can manually modify the file. And replace the default skin.
-            //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
-            let theme = new eui.Theme("resource/default.thm.json", this.stage);
-            theme.addEventListener(eui.UIEvent.COMPLETE, () => {
+    private async loadTheme(){
+        return new Promise((resolve,reject)=>{
+            let theme = new eui.Theme('resource/default.thm.json',this.stage);
+            theme.addEventListener(eui.UIEvent.COMPLETE,()=>{
                 resolve();
-            }, this);
-
+            },this)
         })
     }
 
-    private textfield: egret.TextField;
-    /**
-     * 创建场景界面
-     * Create scene interface
-     */
-    protected createGameScene(): void {
-        let sky = this.createBitmapByName("bg_jpg");
-        this.addChild(sky);
+    protected createGameSence():void{
+        let sky = this.createBitmapByName('bg_jpg');
+        this.addChild(sky)
         let stageW = this.stage.stageWidth;
         let stageH = this.stage.stageHeight;
         sky.width = stageW;
         sky.height = stageH;
 
         let topMask = new egret.Shape();
-        topMask.graphics.beginFill(0x000000, 0.5);
-        topMask.graphics.drawRect(0, 0, stageW, 172);
+        topMask.graphics.beginFill(0x000000,0.5);
+        topMask.graphics.drawRect(0,0,stageW,172);
         topMask.graphics.endFill();
         topMask.y = 33;
         this.addChild(topMask);
 
-        let icon: egret.Bitmap = this.createBitmapByName("egret_icon_png");
+        let icon:egret.Bitmap = this.createBitmapByName('egret_icon_png')
         this.addChild(icon);
         icon.x = 26;
         icon.y = 33;
 
         let line = new egret.Shape();
-        line.graphics.lineStyle(2, 0xffffff);
-        line.graphics.moveTo(0, 0);
-        line.graphics.lineTo(0, 117);
+        line.graphics.lineStyle(2,0xffffff);
+        line.graphics.moveTo(0,0)
+        line.graphics.lineTo(0,117);
         line.graphics.endFill();
         line.x = 172;
         line.y = 61;
         this.addChild(line);
 
-
-        let colorLabel = new egret.TextField();
-        colorLabel.textColor = 0xffffff;
-        colorLabel.width = stageW - 172;
-        colorLabel.textAlign = "center";
-        colorLabel.text = "Hello Egret";
-        colorLabel.size = 24;
-        colorLabel.x = 172;
-        colorLabel.y = 80;
-        this.addChild(colorLabel);
+        let colorLable = new egret.TextField();
+        colorLable.textColor = 0xffffff;
+        colorLable.width = stageW - 172;
+        colorLable.textAlign = 'center';
+        colorLable.text = 'hello egret';
+        colorLable.size = 24;
+        colorLable.x = 172;
+        colorLable.y = 80;
+        this.addChild(colorLable)
 
         let textfield = new egret.TextField();
         this.addChild(textfield);
@@ -151,61 +112,55 @@ class Main extends eui.UILayer {
         this.textfield = textfield;
 
         let button = new eui.Button();
-        button.label = "Click!";
+        button.label = 'Click';
         button.horizontalCenter = 0;
         button.verticalCenter = 0;
         this.addChild(button);
-        button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
-    }
-    /**
-     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
-     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
-     */
-    private createBitmapByName(name: string): egret.Bitmap {
-        let result = new egret.Bitmap();
-        let texture: egret.Texture = RES.getRes(name);
-        result.texture = texture;
-        return result;
-    }
-    /**
-     * 描述文件加载成功，开始播放动画
-     * Description file loading is successful, start to play the animation
-     */
-    private startAnimation(result: Array<any>): void {
-        let parser = new egret.HtmlTextParser();
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onButtonClick,this)
 
-        let textflowArr = result.map(text => parser.parse(text));
+
+
+    }
+
+    private createBitmapByName(name: string):egret.Bitmap{
+        let result = new egret.Bitmap();
+        let texture:egret.Texture = RES.getRes(name);
+        result.texture= texture;
+        return result;
+
+    }
+
+    private onButtonClick(e:egret.TouchEvent){
+        let panel = new eui.Panel();
+        panel.title = 'Title';
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        this.addChild(panel);
+    }
+
+    private startAnimation(resule:Array<any>):void{
+        let parser = new egret.HtmlTextParser();
+        
+        let textflowArr = resule.map(text=>parser.parse(text));
         let textfield = this.textfield;
         let count = -1;
-        let change = () => {
+        let change = ()=>{
             count++;
-            if (count >= textflowArr.length) {
+            if(count >= textflowArr.length){
                 count = 0;
             }
             let textFlow = textflowArr[count];
 
             // 切换描述内容
-            // Switch to described content
             textfield.textFlow = textFlow;
             let tw = egret.Tween.get(textfield);
-            tw.to({ "alpha": 1 }, 200);
+            tw.to({'alpha':1},200);
             tw.wait(2000);
-            tw.to({ "alpha": 0 }, 200);
-            tw.call(change, this);
-        };
+            tw.to({'alpha':0},200);
+            tw.call(change,this);
 
-        change();
+        }
     }
 
-    /**
-     * 点击按钮
-     * Click the button
-     */
-    private onButtonClick(e: egret.TouchEvent) {
-        let panel = new eui.Panel();
-        panel.title = "Title";
-        panel.horizontalCenter = 0;
-        panel.verticalCenter = 0;
-        this.addChild(panel);
-    }
+
 }
